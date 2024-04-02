dd3e35afb46e1f0e64103a9ea291891613c7c70b
PetrKavan31 <petrkavan31@gmail.com>
2024-04-02 20:22:20 +0200

    OOP def student

---
+++ Main.py
@@ -0,0 +1,36 @@
+class Student:
+
+
+  spec ='devOps'
+  def __init__(self, name, age):
+    self.name=name
+    self.age=age
+
+  def showMag(self):
+    print(f'moje jmeno je {self.name} a muj vek je {self.age}')
+
+
+  def sayHi(self,pozdrav):
+    print(f'{pozdrav} ja jsem {self.name}')
+
+student = Student('Adam', 55)
+student2 = Student('Osel', 66)
+
+student.showMag()
+
+student.sayHi('Ahoj')
+student2.sayHi('Dobry den')
+
+
+
+
+
+# student1=Student('Pavel', 2000)
+# print(student1.name)
+#
+# student2 = Student('Adam', 1999)
+# print(student2.age)
+# print(student2.spec)
+#
+# student2.name = 'Petr'
+# print(student2.name)
