b8f597a626ea8921be2087dea5d6458d5c5f8def
PetrKavan31 <petrkavan31@gmail.com>
2024-04-02 21:17:28 +0200

    person // py

--- person.py
+++ person.py
@@ -9,3 +9,18 @@ class Person:
 
 clovek1 = Person('Pavel', 'Novak', 45)
 clovek1.showMsg()
+
+
+class Student(Person):
+    #spec = 'devOps'
+    def __init__(self, name, surname, age, spec):
+        super().__init__(name, surname, age)
+        self.spec=spec
+
+    def showMsg(self):
+        return (f'{super().showMsg()} a specializace je {self.spec}')
+
+student1 = Student('Adam', 'Vesely', 45, 'HW')
+print(student1.spec)
+print(student1.showMsg())
+print(clovek1.showMsg())
