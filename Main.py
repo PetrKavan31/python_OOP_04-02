b45115a88331bbe38c3d679919e64f2a1cfd9486
PetrKavan31 <petrkavan31@gmail.com>
2024-04-05 18:27:06 +0200

    add file Classis

--- Main.py
+++ Main.py
@@ -1,48 +1,48 @@
-#
 
-# class Student:
+
+class Student:
+
+
+  spec ='devOps'
+  def __init__(self, name, age):
+    self.name=name
+    self.age=age
+
+  def showMag(self):
+    print(f'moje jmeno je {self.name} a muj vek je {self.age}')
+
+
+  def sayHi(self,pozdrav):
+    print(f'{pozdrav} ja jsem {self.name}')
+
+
+  def checkAlcohol(self):
+    if self.age>18:
+      rozhodnuti = 'pije'
+    else:
+      rozhodnuti = 'pije'
+    print(rozhodnuti)
+
+student = Student('Adam', 55)
+student2 = Student('Osel', 66)
+
+student.showMag()
+
+student.sayHi('Ahoj')
+student2.sayHi('Dobry den')
+
+student.checkAlcohol()
+
+
+
+
+
+# student1=Student('Pavel', 2000)
+# print(student1.name)
 #
+# student2 = Student('Adam', 1999)
+# print(student2.age)
+# print(student2.spec)
 #
-#   spec ='devOps'
-#   def __init__(self, name, age):
-#     self.name=name
-#     self.age=age
-#
-#   def showMag(self):
-#     print(f'moje jmeno je {self.name} a muj vek je {self.age}')
-#
-#
-#   def sayHi(self,pozdrav):
-#     print(f'{pozdrav} ja jsem {self.name}')
-#
-#
-#   def checkAlcohol(self):
-#     if self.age>18:
-#       rozhodnut = 'pije'
-#     else:
-#       rozhodnut = 'pije'
-#     print(rozhodnut)
-#
-# student = Student('Adam', 55)
-# student2 = Student('Osel', 66)
-#
-# student.showMag()
-#
-# student.sayHi('Ahoj')
-# student2.sayHi('Dobry den')
-#
-# student.checkAlcohol()
-#
-#
-#
-#
-#
-# # student1=Student('Pavel', 2000)
-# # print(student1.name)
-# #
-# # student2 = Student('Adam', 1999)
-# # print(student2.age)
-# # print(student2.spec)
-# #
-# # student2.name = 'Petr'
-# # print(student2.name)
+# student2.name = 'Petr'
+# print(student2.name)
