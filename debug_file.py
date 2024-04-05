aeb854ac2e0f8c5f0ea16e9d35aebe2fc7fb0e27
PetrKavan31 <petrkavan31@gmail.com>
2024-04-05 16:03:12 +0200

    task 1 for debugging

--- debug_file.py
+++ debug_file.py
@@ -1,13 +1,23 @@
 
-veta = 'Na železnici za Čelákovicemi ve směru na Prahu došlo k poruše trakčního vedení, potvrdil pro CNN Prima NEWS tiskový mluvčí Českých drah Filip Medelský. Incident je vyšetřován jako mimořádná událost.'
+# veta = 'Na železnici za Čelákovicemi ve směru na Prahu došlo k poruše trakčního vedení, potvrdil pro CNN Prima NEWS tiskový mluvčí Českých drah Filip Medelský. Incident je vyšetřován jako mimořádná událost.'
+#
+# #1
+# mySet = set()
+# for _ in veta:
+#   mySet.add(_)
+# print(mySet)
+# #mySet = set(veta)
+#
+# #2
+# for _ in mySet:
+#   print(_  + " " + str(veta.count(_)))
 
-#1
-mySet = set()
-for _ in veta:
-  mySet.add(_)
-print(mySet)
-#mySet = set(veta)
 
-#2
-for _ in mySet:
-  print(_  + " " + str(veta.count(_)))
+# task 1 from ITNetwork for debugging
+n = 10
+for i in range(n):
+    for j in range(n):
+        print(i * j, end=" ")
+    print()
+
+# task 2 from ITNetwork for debugging
