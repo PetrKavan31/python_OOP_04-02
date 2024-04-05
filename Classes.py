e48340ae295c2f86b3170010d656e9fea453261f
PetrKavan31 <petrkavan31@gmail.com>
2024-04-05 18:37:23 +0200

    add file Classes - homework

--- Classes.py
+++ Classes.py
@@ -1,2 +1,10 @@
 
-#
+# Implement a class Car. Class fields should store the following:
+# model, year of release, manufacturer, engine capacity,
+# color, price. Implement class methods for data input and output,
+# provide access to individual fields through class methods.
+
+class Cars:
+
+
+    def __init__(self, ):
