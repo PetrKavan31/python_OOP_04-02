fcda57867661dd5abbd83f359080e998dbf8a619
PetrKavan31 <petrkavan31@gmail.com>
2024-04-05 16:33:44 +0200

    task 2 prepare for debugging

--- debug_file.py
+++ debug_file.py
@@ -14,10 +14,29 @@
 
 
 # task 1 from ITNetwork for debugging
-n = 10
-for i in range(n):
-    for j in range(n):
-        print(i * j, end=" ")
-    print()
+# n = 10
+# for i in range(n):
+#     for j in range(n):
+#         print(i * j, end=" ")
+#     print()
+
+
 
 # task 2 from ITNetwork for debugging
+import random
+
+
+def bubble_sort(randomlist):
+    for passnum in range(len(randomlist)-1, 0, -1):
+        for i in range(passnum):
+            if randomlist[i] > randomlist[i + 1]:
+                temp = randomlist[i + 1]
+                randomlist[i] = randomlist[i]
+                randomlist[i + 1] = temp
+
+
+randomlist = random.sample(range(1, 50), 5)
+print(randomlist)
+
+bubble_sort(randomlist)
+print(randomlist)
