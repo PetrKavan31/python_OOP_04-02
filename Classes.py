098ecb98f4dfe42135e79a1170c249f906678e68
PetrKavan31 <petrkavan31@gmail.com>
2024-04-05 18:52:23 +0200

    add file Classes - homework, add cars

--- Classes.py
+++ Classes.py
@@ -4,7 +4,17 @@
 # color, price. Implement class methods for data input and output,
 # provide access to individual fields through class methods.
 
-class Cars:
+class Car:
 
+    def __init__(self, model, year_of_release, manufacturer, engine_capacity, color, price):
+        self.model = model
+        self.year = year_of_release
+        self.manufacturer = manufacturer
+        self.engine = engine_capacity
+        self.color = color
+        self.price = price
 
-    def __init__(self, ):
+    def
+
+car1 = Car("Civic", 2004, "Honda", 1.6, "grey", 1500)
+car2 = Car("Fusion", 2010, "Ford", 1.4, "red", 5000)
