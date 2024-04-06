f7ff33a16c88c91b71de16f71762445afd196b5a
PetrKavan31 <petrkavan31@gmail.com>
2024-04-06 17:48:54 +0200

    add methods

--- Inheritance.py
+++ Inheritance.py
@@ -6,36 +6,44 @@
 # Each class must have the required methods.
 
 class Ship:
-    def __init__(self, name, length, speed ):
+    def __init__(self, name, length, speed):
         self.name = name
         self.length = length
         self.speed = speed
 
     def show_msg(self):
-        print(f"I am warship {self.name}, my length is {self.length} m and my speed is {self.speed} knots.")
+        return (f"I am warship {self.name}, my length is {self.length} m and my speed is {self.speed} knots")
+
 
 class Frigate(Ship):
     def __init__(self, name, length, speed, spec):
-        super.__init__(name, length, speed)
+        super().__init__(name, length, speed)
         self.spec = spec
 
-
-frigate1 = Frigate("USS Leahy", 162, 32, "radars")
+    def show_msg(self):
+        print(f"{super().show_msg()} and my special power is: {self.spec}!")
 
 
 class Destroyer(Ship):
     def __init__(self, name, length, speed, spec):
-        super.__init__(name, length, speed)
+        super().__init__(name, length, speed)
         self.spec = spec
-
-
-destroyer1 = Destroyer("USS Winston S. Churchill", 155, 30, "torpedoes")
-
+    def show_msg(self):
+        print(f"{super().show_msg()} and my special power is: {self.spec}!")
 
 class Cruiser(Ship):
     def __init__(self, name, length, speed, spec):
-        super.__init__(name, length, speed)
+        super().__init__(name, length, speed)
         self.spec = spec
 
+    def show_msg(self):
+        print(f"{super().show_msg()} and my special power is: {self.spec}!")
 
+
+frigate1 = Frigate("USS Leahy", 162, 32, "radars")
+destroyer1 = Destroyer("USS Winston S. Churchill", 155, 30, "torpedoes")
 cruiser1 = Cruiser("USS Arkansas", 178, 30, "missiles")
+
+frigate1.show_msg()
+destroyer1.show_msg()
+cruiser1.show_msg()
