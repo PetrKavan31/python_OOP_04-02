41fdc70dfa7d5c108442e8c020887baf94b662f7
PetrKavan31 <petrkavan31@gmail.com>
2024-04-06 16:58:27 +0200

    add frigate, destroyer and cruise class

--- Inheritance.py
+++ Inheritance.py
@@ -5,3 +5,37 @@
 # a destroyer), a Cruiser class (contains info about a cruiser).
 # Each class must have the required methods.
 
+class Ship:
+    def __init__(self, name, length, speed ):
+        self.name = name
+        self.length = length
+        self.speed = speed
+
+    def show_msg(self):
+        print(f"I am warship {self.name}, my length is {self.length} m and my speed is {self.speed} knots.")
+
+class Frigate(Ship):
+    def __init__(self, name, length, speed, spec):
+        super.__init__(name, length, speed)
+        self.spec = spec
+
+
+frigate1 = Frigate("USS Leahy", 162, 32, "radars")
+
+
+class Destroyer(Ship):
+    def __init__(self, name, length, speed, spec):
+        super.__init__(name, length, speed)
+        self.spec = spec
+
+
+destroyer1 = Destroyer("USS Winston S. Churchill", 155, 30, "torpedoes")
+
+
+class Cruiser(Ship):
+    def __init__(self, name, length, speed, spec):
+        super.__init__(name, length, speed)
+        self.spec = spec
+
+
+cruiser1 = Cruiser("USS Arkansas", 178, 30, "missiles")
