05ab625e6f9a01343b66d851186dfc07c425a834
PetrKavan31 <petrkavan31@gmail.com>
2024-04-06 15:03:20 +0200

    new file - inheritance

---
+++ Inheritance.py
@@ -0,0 +1,7 @@
+
+# Create a Ship class containing information about a ship.
+# Use inheritance to implement a Frigate class (contains
+# info about a frigate), a Destroyer class (contains info about
+# a destroyer), a Cruiser class (contains info about a cruiser).
+# Each class must have the required methods.
+
