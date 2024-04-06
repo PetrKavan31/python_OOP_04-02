61120a209b24f6ee763b45f455e49b8bb16fb49c
PetrKavan31 <petrkavan31@gmail.com>
2024-04-06 10:59:16 +0200

    add methods

--- Classes.py
+++ Classes.py
@@ -17,9 +17,18 @@ class Car:
     def show_car(self):
         print("My car:\n", self.model, self.year, self.manufacturer, self.engine, self.color, self.price)
 
+    def show_owner(self, text_msg):
+        return f"Owner car {self.manufacturer} {self.model} is {text_msg}"
+
 
 car1 = Car("Civic", 2004, "Honda", 1.6, "grey", 1500)
 car2 = Car("Fusion", 2010, "Ford", 1.4, "red", 5000)
 
 car1.show_car()
 car2.show_car()
+
+print(car1.model)
+print(car2.color)
+
+print(car1.show_owner("Petr"))
+print(car2.show_owner("Marketa"))
