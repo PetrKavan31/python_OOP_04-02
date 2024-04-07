4699a65c3b77df62df1c7c6dea6cfe0673c70baa
PetrKavan31 <petrkavan31@gmail.com>
2024-04-07 20:36:46 +0200

    new file oop_home, ITStep lesson, examples

--- oop_home.py
+++ oop_home.py
@@ -1,12 +1,17 @@
+import random
 
 class Person:
     def __init__(self, name, surname, age):
         self.name = name
         self.surname = surname
         self.age = age
+        self.__person_id = random.randint(1,100)
+
+    def __get_id(self):
+        return f"{self.__person_id}\n"
 
     def get_info(self):
-        return f"Person first name - {self.name}; last name - {self.surname}; age {self.age}."
+        return f"{self.__get_id()} Person first name - {self.name}; last name - {self.surname}; age {self.age}."
 
     def get_hi(self, text_msg):
         person_hi = self.get_info()
@@ -14,6 +19,40 @@ class Person:
 
 
 person1 = Person("Joe", "Black", 30)
-print(person1.get_hi("Hi"))
-person1.age = 35
-print(person1.get_hi("Hi"))
+# print(person1.get_hi("Hi"))
+# person1.age = 35
+# print(person1.get_hi("Hi"))
+# person1.__person_id = 100
+# print(person1.get_info())
+
+
+class Persson:
+    def __init__(self, firstName, lastName, age):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+
+    def getInfo(self):
+        return f"Person first name -  {self.firstName}; last name - {self.lastName}; age - {self.age}."
+
+    def getHi(self, msgText):
+        return f"{msgText}! I am {self.firstName}."
+
+
+persson1=Persson("Joe","Black",30)
+persson2=Persson("Kate","Smith",20)
+print(persson1.getInfo())
+print(persson2.getInfo())
+print(persson1.getHi("Hi"))
+print(persson2.getHi("Hello"))
+
+
+class Student(Persson):
+    spec="Computer Science"
+    def isSuccessful(self,meanScore):
+        return True if meanScore>=75 else False
+
+student1=Student("Joe","Black",30)
+print(student1.getInfo())
+print(student1.getHi("Morning"))
+print(f"Is {student1.firstName} successful student?.{student1.isSuccessful(85)}")
